833c753e2a4aadd92acc0cc41d0d82b78962c7c4
Ilan Olkies <ilanolkies@outlook.com>
2018-06-08 16:24:23 -0300

    Remove comments.

--- CleanUp/App.xaml.cs
+++ CleanUp/App.xaml.cs
@@ -1,11 +1,10 @@
-using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
 namespace CleanUp
 {
-    public partial class App : Application
+	public partial class App : Application
     {
         public App()
         {
@@ -16,17 +15,14 @@ namespace CleanUp
 
         protected override void OnStart()
         {
-            // Handle when your app starts
         }
 
         protected override void OnSleep()
         {
-            // Handle when your app sleeps
         }
 
         protected override void OnResume()
         {
-            // Handle when your app resumes
         }
     }
 }
